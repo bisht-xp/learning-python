cb517284f35c13c35e6a359edcbb8ae8f7217def
bisht-xp <bishtkamal2907@gmail.com>
2025-08-19 21:46:53 +0530

    add: added binary search card

---
+++ 15_Monotonic_Queue/search_roated_sorted_array.py
@@ -0,0 +1,20 @@
+def search(nums: list[int], target: int) -> int:
+    left,right=0,len(nums)-1
+
+    while left < right:
+        mid = (left+right) // 2
+
+        if(nums[0] <= nums[mid]):
+            if(nums[0] <= target <= nums[mid]):
+                right = mid
+            else:
+                left = mid+1
+        else:
+            if(nums[mid] < target <= nums[-1]):
+                left = mid+1
+            else:
+                right = mid
+    
+    return left if nums[left] == target else -1
+
+print(search([4,5,6,7,0,1,2], 0))
