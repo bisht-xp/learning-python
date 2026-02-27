08617e38c5b223dc4cea64f82b7eb9b9b93a8b50
bisht-xp <bishtkamal2907@gmail.com>
2026-02-27 17:17:35 +0530

    add: added setMatrix zero problem

---
+++ 25_Math_Geometry/setMatix_zero.py
@@ -0,0 +1,28 @@
+def setZeroes( matrix: list[list[int]]) -> None:
+    rows,cols = len(matrix),len(matrix[0])
+    rowZero = False
+
+    for r in range(rows):
+        for c in range(cols):
+            if matrix[r][c] == 0:
+                matrix[0][c] = 0
+                if r > 0:
+                    matrix[r][0] = 0
+                else:
+                    rowZero = True
+    
+
+    for r in range(1, rows):
+        for c in range(1, cols):
+            if not matrix[r][0] or not matrix[0][c]:
+                matrix[r][c] = 0
+    
+
+    if not matrix[0][0]:
+        for r in range(rows):
+            matrix[r][0] = 0
+    
+    if rowZero:
+        for c in range(cols):
+            matrix[0][c] = 0
+    
